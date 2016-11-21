8aa6878f4d7d4d941973b8419ba6c1b57df2e7ed
Roula Alhaddad <roulahd85@gmail.com>
2016-11-21 12:00:32 +0100

    Add Entity(Model) Classes

---
+++ TaskManager/TaskManager/Models/Assignment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Models
+{
+    public class Assignment
+    {
+        public int Assignment_Id { get; set; }
+        public String Assignment_Name { get; set; }
+        public string Client { get; set; }
+        public String Percentage { get; set; }
+
+        public DateTime Start_date { get; set; }
+
+        public DateTime  End_date { get; set; }
+        public string Comment { get; set; }
+        public int Consultant_Id { get; set; }
+        public virtual Consultant Consultant { get; set; }
+    }
+}
---
+++ TaskManager/TaskManager/Models/Consultant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Models
+{
+    public class Consultant
+    {
+        public int Consultant_Id { get; set; }
+        public string Consultant_Name { get; set; }
+
+        public virtual List<Assignment> Assignments { get; set; }
+    }
+}
